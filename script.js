41ceb91e345a0ce4c8a57ca872b69b8a67e66c74
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-16 14:44:18 +0100

    Character anzeigen

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 let canvas;
-let ctx;
-
-let world = new World();
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext("2d");
-
+    world = new World(canvas);
     console.log('My Character is', world.character);
-
-/*     Das war zur Probe: character.src = './img/2_character_pepe/2_walk/W-21.png';
-        ctx.drawImage(character, 20, 20, 50, 150); */
-
 }
