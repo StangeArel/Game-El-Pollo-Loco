eed7ca18cc156e97c635e43f98941e9a6832bc53
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-18 23:52:20 +0100

    Hintergrundobjekte einfügen

--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,10 @@ class World {
         new Cloud()
     ];
     backgroundObject = [
-        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0)
+        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 0),
+
     ];
     canvas;
     ctx;
