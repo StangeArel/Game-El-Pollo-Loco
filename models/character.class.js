1ba759795e9b56c53f4a6384cb19fb176fb757ee
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-19 22:12:56 +0100

    Kamera verschieben

--- models/character.class.js
+++ models/character.class.js
@@ -33,6 +33,8 @@ class Character extends MovableObject {
                 this.otherDirection = true;
             }
 
+            this.world.camera_x = -this.x;
+
         }, 1000 / 60);
 
         setInterval(() => {
