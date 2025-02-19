b37ecac7b9c92191049e441335b57afac4753fa9
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-19 22:36:11 +0100

    Level Objekt

--- models/world.class.js
+++ models/world.class.js
@@ -1,39 +1,8 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObject = [
-        new BackgroundObject('../img/5_background/layers/air.png', -719),
-        new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('../img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', -719),
-
-        new BackgroundObject('../img/5_background/layers/air.png', 0),
-        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 0),
-
-        new BackgroundObject('../img/5_background/layers/air.png', 719),
-        new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', 719),
-        new BackgroundObject('../img/5_background/layers/2_second_layer/2.png',719),
-        new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', 719),
-
-        new BackgroundObject('../img/5_background/layers/air.png', 719*2),
-        new BackgroundObject('../img/5_background/layers/3_third_layer/1.png', 719*2),
-        new BackgroundObject('../img/5_background/layers/2_second_layer/1.png', 719*2),
-        new BackgroundObject('../img/5_background/layers/1_first_layer/1.png', 719*2),
-
-        new BackgroundObject('../img/5_background/layers/air.png', 719*3),
-        new BackgroundObject('../img/5_background/layers/3_third_layer/2.png', 719*3),
-        new BackgroundObject('../img/5_background/layers/2_second_layer/2.png',719*3),
-        new BackgroundObject('../img/5_background/layers/1_first_layer/2.png', 719*3)
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObject = level1.backgroundObject;
 
     canvas;
     ctx;
@@ -77,7 +46,7 @@ class World {
     }
 
     addToMap(mo) {
-        if(mo.otherDirection) {
+        if (mo.otherDirection) {
             this.ctx.save();
             this.ctx.translate(mo.width, 0);
             this.ctx.scale(-1, 1);
@@ -86,7 +55,7 @@ class World {
 
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
 
-        if(mo.otherDirection) {
+        if (mo.otherDirection) {
             mo.x = mo.x * -1;
             this.ctx.restore();
         }
