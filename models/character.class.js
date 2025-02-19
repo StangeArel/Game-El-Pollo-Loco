ba2950b7dcff4ee908226c5eb329d88763f32f3c
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-19 11:06:25 +0100

    ImageCache 0.1

--- models/character.class.js
+++ models/character.class.js
@@ -1,12 +1,10 @@
 class Character extends MovableObject{
 
-    height = 280;
-    y = 155;
+    height = 250;
+    y = 180;
 
     constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
-        this.x;
-        this.y;
     }
 
     jump() {
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,13 +4,23 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = [];
 
+    // loadImage('img/test.png')
     loadImage(path) {
-        this.img = new Image();
+        this.img = new Image();  // this.img = document.getElementById('image') <img ig="image" src>
         this.img.src = path;
-        this.img.onload = () => {
-            console.log(`Bild geladen: ${path}`)
-        }
+        /*         this.img.onload = () => {
+                    console.log(`Bild geladen: ${path}`)
+                } */
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache.push(img);
+        });
     }
 
     moveRight() {
