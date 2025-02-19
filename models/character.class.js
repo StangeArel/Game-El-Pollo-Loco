feb0f49a2830d9507a71e8b44291330649597062
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-19 12:54:02 +0100

    Setze zurück, wenn das letzte Bild erreicht ist

--- models/character.class.js
+++ models/character.class.js
@@ -1,18 +1,34 @@
-class Character extends MovableObject{
+class Character extends MovableObject {
 
     height = 250;
     y = 180;
+    IMAGES_WALKING = [
+        '../img/2_character_pepe/2_walk/W-21.png',
+        '../img/2_character_pepe/2_walk/W-22.png',
+        '../img/2_character_pepe/2_walk/W-23.png',
+        '../img/2_character_pepe/2_walk/W-24.png',
+        '../img/2_character_pepe/2_walk/W-25.png',
+        '../img/2_character_pepe/2_walk/W-26.png'
+    ];
+    currentImage = 0;
 
     constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
-            '../img/2_character_pepe/2_walk/W-21.png',
-            '../img/2_character_pepe/2_walk/W-22.png',
-            '../img/2_character_pepe/2_walk/W-23.png',
-            '../img/2_character_pepe/2_walk/W-24.png',
-            '../img/2_character_pepe/2_walk/W-25.png',
-            '../img/2_character_pepe/2_walk/W-26.png'
-        ]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[this.currentImage];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+
+/*             if (this.currentImage >= this.IMAGES_WALKING.length) { // Setze zurück, wenn das letzte Bild erreicht ist
+                this.currentImage = 0; */
+
+            }, 1000);
     }
 
     jump() {
