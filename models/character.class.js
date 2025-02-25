2245f74ea56830dbf2341baa8aec8ff064acddc7
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-25 22:20:36 +0100

    idle & longIdle animation

--- models/character.class.js
+++ models/character.class.js
@@ -40,6 +40,32 @@ class Character extends MovableObject {
         'img/2_character_pepe/4_hurt/H-43.png'
     ]
 
+    IMAGES_IDLE = [
+        'img/2_character_pepe/1_idle/idle/I-1.png',
+        'img/2_character_pepe/1_idle/idle/I-2.png',
+        'img/2_character_pepe/1_idle/idle/I-3.png',
+        'img/2_character_pepe/1_idle/idle/I-4.png',
+        'img/2_character_pepe/1_idle/idle/I-5.png',
+        'img/2_character_pepe/1_idle/idle/I-6.png',
+        'img/2_character_pepe/1_idle/idle/I-7.png',
+        'img/2_character_pepe/1_idle/idle/I-8.png',
+        'img/2_character_pepe/1_idle/idle/I-9.png',
+        'img/2_character_pepe/1_idle/idle/I-10.png'
+    ]
+
+    IMAGES_LONG_IDLE = [
+        'img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'img/2_character_pepe/1_idle/long_idle/I-20.png'
+    ]
+
     world;
 
     maxAvailableBottles = 10;
@@ -51,6 +77,8 @@ class Character extends MovableObject {
         this.loadImages(this.IMAGES_JUMPING);
         this.loadImages(this.IMAGES_DEAD);
         this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_LONG_IDLE);
         this.applyGravity(); // Gravitation
         this.animate();
     }
@@ -79,19 +107,33 @@ class Character extends MovableObject {
         }, 1000 / 60);
 
         setInterval(() => {
+            console.log(this.longIdle);
             if (this.isDead()) {
-                this.playAnimation(this.IMAGES_DEAD);
+                this.playAnimationOnce(this.IMAGES_DEAD);
+                this.longIdle = false;
             } else if (this.isHurt()) {
                 this.playAnimation(this.IMAGES_HURT);
+                this.longIdle = false;
             } else if (this.isAboveGround()) {
                 this.playAnimation(this.IMAGES_JUMPING);
-            } else {
-                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                    // Walk animation
-                    this.playAnimation(this.IMAGES_WALKING);
+                this.longIdle = false;
+            } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.playAnimation(this.IMAGES_WALKING);
+                this.longIdle = false;
+            } else if (this.longIdle) {
+                this.playAnimation(this.IMAGES_LONG_IDLE);
+            }  else {
+                this.playAnimation(this.IMAGES_IDLE);
+                this.longIdle = false;
+
+                if (!this.longIdleTimer) {
+                    this.longIdleTimer = setTimeout(() => {
+                        this.longIdle = true;
+                        this.longIdleTimer = null;
+                    }, 2000);
                 }
-            }
-        }, 50);
+            }    
+        }, 70);
 
         setInterval(() => {
             this.currentAvailableBottles = Math.min(this.currentAvailableBottles + 1, this.maxAvailableBottles);
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -58,6 +58,13 @@ class MovableObject extends DrawableObject {
         this.currentImage++;
     }
 
+    playAnimationOnce(images) {
+        let i = this.currentImage % images.length;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
     moveRight() {
         this.x += this.speed;
     }
