47b1c23cc33012949ed694cc3102162986e2f83d
Valeriya Stange <valeriya.stange@gmail.com>
2025-03-01 10:55:44 +0100

    Start screen & Button

--- script.js
+++ script.js
@@ -4,53 +4,22 @@ let keyboard = new KeyBoard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas, keyboard);
-
-    console.log('My Character is', world.character);
+    showStartScreen();
+    //startGame();
 }
 
-document.addEventListener("keydown", (e) => {
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = true;
-    }
+function showStartScreen() {
+    let context = canvas.getContext('2d');
+    let img = new Image();
+    img.src = './img/9_intro_outro_screens/start/startscreen_1.png';
 
-    if (e.keyCode == 37) {
-        keyboard.LEFT = true;
-    }
+    img.onload = function() {
+        context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+}
 
-    if (e.keyCode == 38) {
-        keyboard.UP = true;
-    }
-
-    if (e.keyCode == 40) {
-        keyboard.DOWN = true;
-    }
-
-    if (e.keyCode == 32) {
-        keyboard.SPACE = true;
-    }
-//    console.log(e);
-});
-
-
-document.addEventListener("keyup", (e) => {
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = false;
-    }
-
-    if (e.keyCode == 37) {
-        keyboard.LEFT = false;
-    }
-
-    if (e.keyCode == 38) {
-        keyboard.UP = false;
-    }
-
-    if (e.keyCode == 40) {
-        keyboard.DOWN = false;
-    }
-
-    if (e.keyCode == 32) {
-        keyboard.SPACE = false;
-    }
-});
+function startGame() {
+    world = new World(canvas, keyboard);
+    let btnStart = document.getElementById('btnStartGame');
+    btnStart.classList.toggle("d_none");
+}
